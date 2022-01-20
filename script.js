74db9fc5b4845b86b1f704f9f406231ec6498196
one23four56 <jason@baydq.com>
2022-01-19 21:02:13 -0600

    switch to javascript only

---
+++ script.js
@@ -0,0 +1,23 @@
+const q = (query) => document.querySelector(query);
+const events = [
+    () => {
+        alert(1);
+    },
+    () => {
+        alert(2);
+    },
+    () => {
+        // you get the idea just keep adding on to this 
+    }
+];
+function getRandom(array) {
+    const number = Math.floor(Math.random() * array.length);
+    return array[number];
+}
+window.addEventListener("load", event => {
+    q("#centerButton").style.opacity = "1";
+    q("h1").style.opacity = "1";
+    q("#centerButton").addEventListener("click", event => {
+        getRandom(events)();
+    });
+});
