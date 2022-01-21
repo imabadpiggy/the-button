25318cef6d4afd4084e36908d0a5209e672f0295
imabadpiggy <jonny@baydq.com>
2022-01-20 19:30:18 -0600

    add sounds

--- script.js
+++ script.js
@@ -2,14 +2,18 @@ const q = (query) => document.querySelector(query);
 
 const events = [
     () => {
-        alert(1);
+        q("#centerButton").style.backgroundColor = "blue";
+        q("#centerButton").style.borderColor = "darkblue"
     },
     () => {
-        alert(2);
+        q("#vineBoom").play();
     },
     () => {
-        // you get the idea just keep adding on to this 
-    }
+        document.body.style.backgroundImage = "url(https://i5.walmartimages.com/asr/aab6c643-8504-4b82-9a21-14784426d81d_1.bbadfd6a062bda13151d6486233c516b.jpeg)"
+    },
+    () => {
+        q("#cr").play();
+    },
 ];
 
 function getRandom(array) {
@@ -18,8 +22,10 @@ function getRandom(array) {
 }
 
 window.addEventListener("load", event => {
-    q("#centerButton").style.opacity = "1";
     q("h1").style.opacity = "1";
+    setTimeout(() => {
+        q("#centerButton").style.opacity = "1";
+    }, 800);
     q("#centerButton").addEventListener("click", event => {
         getRandom(events)();
     });
